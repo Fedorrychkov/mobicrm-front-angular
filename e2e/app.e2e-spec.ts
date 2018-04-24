cfe65d4bca1b3fc90e5f6ee89e19b4e34acf18ed
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-24 15:27:48 +0300

    ng init

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('mobicrm-angular App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
