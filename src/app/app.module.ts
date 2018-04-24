c7fea6663efbba252e3184cc9f3f8df97829721d
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-24 18:03:05 +0300

    Add normilizecss, fonts, icons, init anguar-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -33,6 +34,7 @@ const APP_COMPONENT = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
