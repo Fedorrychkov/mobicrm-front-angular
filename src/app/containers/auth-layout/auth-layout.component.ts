25bdb970585e6500ad7ddb4d966724cd97d6ec9f
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-24 17:35:45 +0300

    update layouts component

--- src/app/containers/auth-layout/auth-layout.component.ts
+++ src/app/containers/auth-layout/auth-layout.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    templateUrl: 'auth-layout.component.html'
+    templateUrl: 'auth-layout.component.html',
+    styleUrls: ['auth-layout.component.scss']
 })
 
 export class AuthLayoutComponent implements OnInit {
--- src/app/containers/full-layout/full-layout.component.ts
+++ src/app/containers/full-layout/full-layout.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    templateUrl: 'full-layout.component.html'
+    templateUrl: 'full-layout.component.html',
+    styleUrls: ['full-layout.component.scss']
 })
 
 export class FullLayoutComponent implements OnInit {
