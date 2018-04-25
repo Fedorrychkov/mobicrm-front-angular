3aa91cff393f7222f0049bab186b4228fdbb961a
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-25 14:06:29 +0300

    Update app routing module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -24,6 +24,26 @@ export const routes: Routes = [
       component: FullLayoutComponent,
       loadChildren: './views/core/dashboard/dashboard.module#DashboardModule',
     },
+    {
+      path: 'companies',
+      component: FullLayoutComponent,
+      loadChildren: './views/core/companies/companies.module#CompaniesModule',
+    },
+    {
+      path: 'employees',
+      component: FullLayoutComponent,
+      loadChildren: './views/core/employees/employees.module#EmployeesModule',
+    },
+    {
+      path: 'customers',
+      component: FullLayoutComponent,
+      loadChildren: './views/core/customers/customers.module#CustomersModule',
+    },
+    {
+      path: 'orders',
+      component: FullLayoutComponent,
+      loadChildren: './views/core/orders/orders.module#OrdersModule',
+    },
   ];
   
   @NgModule({
