d0c08ac71d2b009b139928dc3f63253274c1c5cc
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-25 23:27:06 +0300

    Update login component

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -11,6 +11,8 @@ export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
   public login: String;
   public password: String;
+  public errorMessage: String;
+  public isLoading = false;
 
   constructor(
     public fb: FormBuilder,
@@ -18,11 +20,16 @@ export class LoginComponent implements OnInit {
   ) { }
   
   auth(data) {
+    this.isLoading = true;
+    this.errorMessage = '';
     const login = data.login;
     const password = data.password;
     this.authService.login(login, password)
       .then((data) => {
-        console.log(data);
+        this.isLoading = false;
+        if (data.status !== 200) {
+          this.errorMessage = data.body.textError;
+        }
       });
   }
 
