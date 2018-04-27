5abb275c8cf9937b8d1a2f4de2b6bb3c30bc8a03
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-27 22:37:26 +0300

    add new order component for companies root component

---
+++ src/app/views/core/companies/company/company.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../../../services';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.scss']
+})
+export class CompanyComponent implements OnInit {
+  private headerNav: any[];
+  private id: string;
+
+  constructor(
+    public route: ActivatedRoute,
+    public eventService: EventService
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+    
+    this.headerNav = [
+      {
+          link: `${this.id}/orders`,
+          name: 'Заказы'
+      },
+      {
+          link: `${this.id}/customers`,
+          name: 'Клиенты'
+      },
+      {
+          link: `${this.id}/employees`,
+          name: 'Сотрудники'
+      },
+  ];
+    this.eventService.broadcast('app-header-nav', {nav: this.headerNav});
+  }
+
+}
