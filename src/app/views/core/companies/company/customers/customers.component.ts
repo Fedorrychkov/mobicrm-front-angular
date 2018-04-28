12e91ed141b94b4d22183a4754534e36faf2b92c
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-28 16:48:17 +0300

    Update customers component

--- src/app/views/core/companies/company/customers/customers.component.ts
+++ src/app/views/core/companies/company/customers/customers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CustomerService } from '../../../../../services';
+import { CustomerService, EventService, CompanyService } from '../../../../../services';
 
 @Component({
   selector: 'app-customers',
@@ -7,16 +7,25 @@ import { CustomerService } from '../../../../../services';
   styleUrls: ['./customers.component.scss']
 })
 export class CustomersComponent implements OnInit {
+  private companyId: string;
 
   constructor(
-    private customerService: CustomerService
+    private customerService: CustomerService,
+    private eventService: EventService,
+    private companyService: CompanyService
   ) { }
-
-  ngOnInit() {
-    this.customerService.getCustomers()
-      .then(data => {
-        console.log(data);
+  getCustomers() {
+    this.customerService.getCustomers(this.companyId)
+      .then( data => {
+        console.log('customers -->', data);
       });
+  }
+  ngOnInit() {
+    this.companyId = this.companyService.companyId;
+    this.getCustomers();
+    this.eventService.on('customer-list-update').subscribe( () => {
+      this.getCustomers();
+    });
   }
 
 }
