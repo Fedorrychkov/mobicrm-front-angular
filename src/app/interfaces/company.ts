410d8df64e672a98e71a2c78f32dafc6caa51c49
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-28 16:46:21 +0300

    Add company interfaces

---
+++ src/app/interfaces/company.ts
@@ -0,0 +1,18 @@
+export interface ICompany {
+    address?: string,
+    avatar?: string,
+    createdAt: string,
+    description?: string,
+    director_id: number,
+    id: number,
+    name?: string,
+    status?: string,
+    tags?: string,
+    updatedAt: string
+  }
+  
+export interface ICompanies {
+    body?: ICompany[],
+    length?: number
+  }
+  
