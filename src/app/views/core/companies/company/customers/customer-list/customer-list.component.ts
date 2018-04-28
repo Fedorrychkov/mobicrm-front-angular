b610a70d58683b81af5669ac7238f141f02153ba
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-29 01:43:35 +0300

    update customer-list component, add data table

--- src/app/views/core/companies/company/customers/customer-list/customer-list.component.ts
+++ src/app/views/core/companies/company/customers/customer-list/customer-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ICustomers } from '../../../../../../interfaces/customer';
+// import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-customer-list',
@@ -7,6 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
+  public _customers: ICustomers;
+  public isLoaded = false;
+
+  public displayedColumns = ['phone', 'name', 'created'];
+
+  @Input()
+  /**
+   * get list of companies
+   */
+  get customers() {
+    return this._customers;
+  }
+
+  /**
+   * set list of companies
+   * @param list list of companies
+   */
+  set customers(list: ICustomers) {
+    this._customers = list;
+    this.isLoaded = true;
+  }
   constructor() { }
 
   ngOnInit() {
