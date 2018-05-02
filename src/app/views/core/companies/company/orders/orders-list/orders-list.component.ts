8e6e8bff1e02fadadac23db7331119dc8b6b7af3
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-02 23:54:06 +0300

    Update order list component, update order component

--- src/app/views/core/companies/company/orders/orders-list/orders-list.component.ts
+++ src/app/views/core/companies/company/orders/orders-list/orders-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IOrders } from '../../../../../../interfaces/orders';
 
 @Component({
   selector: 'app-orders-list',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersListComponent implements OnInit {
 
+  public _orders: IOrders;
+  public isLoaded = false;
+
+  // public displayedColumns = ['phone', 'name', 'created'];
+
+  @Input()
+  /**
+   * get list of companies
+   */
+  get orders() {
+    return this._orders;
+  }
+
+  /**
+   * set list of companies
+   * @param list list of companies
+   */
+  set orders(list: IOrders) {
+    this._orders = list;
+    this.isLoaded = true;
+  }
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
--- src/app/views/core/companies/company/orders/orders.component.ts
+++ src/app/views/core/companies/company/orders/orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { OrderService } from '../../../../../services';
+import { OrderService, EventService, CustomerService, CompanyService } from '../../../../../services';
 
 @Component({
   selector: 'app-orders',
@@ -7,16 +7,31 @@ import { OrderService } from '../../../../../services';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent implements OnInit {
+  private companyId: string;
+  public orders: any;
+  public isLoaded = false;
 
   constructor(
+    private customerService: CustomerService,
+    private eventService: EventService,
+    private companyService: CompanyService,
     private orderService: OrderService
   ) { }
 
-  ngOnInit() {
-    this.orderService.getOrders()
-      .then(data => {
+  getOrders() {
+    this.orderService.getOrders(this.companyId)
+      .then( data => {
         console.log(data);
+        this.orders = data;
+        this.isLoaded = true;
       });
+  }
+  ngOnInit() {
+    this.companyId = this.companyService.companyId;
+    this.getOrders();
+    this.eventService.on('order-list-update').subscribe( () => {
+      this.getOrders();
+    });
   }
 
 }
