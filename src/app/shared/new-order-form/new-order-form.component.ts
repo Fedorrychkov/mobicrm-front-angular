6ff10d41e381d6dfe6fac4d2d8805b6ee6d6e95b
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-02 23:56:23 +0300

    Add create order form, add new fields

--- src/app/shared/new-order-form/new-order-form.component.ts
+++ src/app/shared/new-order-form/new-order-form.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { EventService, AuthService, CompanyService, OrderService } from '../../services';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { EventService, AuthService, CompanyService, OrderService, CustomerService } from '../../services';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ICustomer } from '../../interfaces/customer';
+import { IOrder } from '../../interfaces/orders';
 
 @Component({
   selector: 'app-new-order-form',
@@ -14,16 +16,39 @@ export class NewOrderFormComponent implements OnInit{
   public name: string;
   public address: string;
   public description: string;
-  public customerPhone: string[];
+  public customerPhone: string;
+  public customerFName: string;
+  public customerLName: string;
+  public customerEmail: string;
+  public customerAddress: string;
   public errorMessage: string;
   public isLoading = false;
   public id: string;
+  public fillCustomer = false;
+  public isNewCustomer = false;
+
+  public customer: ICustomer = {
+    id: '',
+    company_id: '',
+    phone: '',
+    first_name: '',
+    last_name: '',
+    address: ''
+  };
+  public order: IOrder = {
+    name: '',
+    address: '',
+    description: '',
+    status: 'NEW',
+    customer: this.customer
+  };
 
   constructor(
     public fb: FormBuilder,
-    public authService: AuthService,
-    public eventService: EventService,
-    public orderService:OrderService,
+    private authService: AuthService,
+    private eventService: EventService,
+    private orderService: OrderService,
+    private customerService: CustomerService,
     public router: Router,
     public dialogRef: MatDialogRef<NewOrderFormComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
@@ -36,36 +61,79 @@ export class NewOrderFormComponent implements OnInit{
     this.dialogRef.close();
   }
 
-  createOrder(data) {
-    console.log(data);
-    // this.orderService.createOrder(data)
-    //   .then( (data) => {
-    //     // this.router.navigate([`companies/${data.id}/dashboard`]);
-    //     // this.dialogRef.close();
-    //   });
+  createOrder(req, valid: boolean) { // TODO: Дописать возможность выбора исполнителя. Исполнитель может быть назначен сразу или позже
+    if(!valid) return;
+    req.company_id = this.data.company_id;
+    const newReq = this.fillCreateRequest(req);
+    this.orderService.createOrder(newReq)
+      .then( (data) => {
+        data.status === 400 ? 
+          this.eventService.broadcast('notification', {type: 'error', message: data.body.errorText})
+          : this.dialogRef.close();
+        this.eventService.broadcast('notification', {type: 'success', message: 'Новый заказ создан!'});
+        this.eventService.broadcast('order-list-update');
+      });
   }
+  
+  checkCustomer(req) {
+    if (!this.customerPhone) {
+      this.removeCustomerFields();
+      return;
+    }
+    this.eventService.broadcast('notification', {type: 'warning', message: 'Ведется поиск клиента по телефону'});
+    this.customerService.checkCustomer(req.customerPhone)
+      .then( res => {
+        this.fillCustomer = true;
+        if (res.status === 200) {
+          this.eventService.broadcast('notification', {type: 'success', message: 'Такой клиент есть!'});
+          this.isNewCustomer = false;
+          this.fillCustomerFields(res.body);
+          return;
+        }
+        this.isNewCustomer = true;
+        this.eventService.broadcast('notification', {type: 'success', message: 'О! Новый клиент? Отлично!'});
+      });
+  }
+  
+  fillCreateRequest(req): IOrder {
+    let newReq = this.order;
+    newReq.name = this.name;
+    newReq.address = this.address;
+    newReq.description = this.description;
+    newReq.customer.phone = this.customerPhone;
+    newReq.customer.first_name = this.customerFName;
+    newReq.customer.last_name = this.customerLName;
+    newReq.customer.email = this.customerEmail;
+    newReq.customer.address = this.customerAddress;
+    return newReq;
+  }
+
+  fillCustomerFields(res: ICustomer) {
+    this.customerAddress = res.address;
+    this.customerEmail = res.email;
+    this.customerFName = res.first_name;
+    this.customerLName = res.last_name;
+    this.address = res.address;
+  }
+  
+  removeCustomerFields() {
+    this.customerAddress = '';
+    this.customerEmail = '';
+    this.customerFName = '';
+    this.customerLName = '';
+  }
+
   ngOnInit() {
     this.newOrderForm = this.fb.group({
-      name: [this.name],
-      description: [this.description],
-      address: [this.address],
-      customerPhone: [this.customerPhone]
+      name: [this.name, [ Validators.required]],
+      description: [this.description, [ Validators.required]],
+      address: [this.address, [ Validators.required]],
+      customerPhone: [this.customerPhone, [ Validators.required]],
+      customerFName: [this.customerFName, [ Validators.required]],
+      customerLName: [this.customerLName, [ Validators.required]],
+      customerEmail: [this.customerEmail, [ Validators.required]],
+      customerAddress: [this.customerAddress, [ Validators.required]]
     });
   }
 
 }
-
-// {
-// 	"company_id": 1,
-// 	"name": "Test Order",
-// 	"address": "г. Казань ул. Кремлёвская д. 35 кв 38",
-// 	"longitude": "55.792414",
-// 	"latitude": "49.120081",
-// 	"description": "Заказчик хочет провести замер окон",
-// 	"status": "NEW",
-// 	"customer": {
-// 		"first_name": "RyR",
-// 		"phone": "+723121120121",
-// 		"email": "ffffff@ff.ru"
-// 	}
-// }
