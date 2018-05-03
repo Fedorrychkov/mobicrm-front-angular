7335e49c7e46cb55737ac47ed1575912be74b2ac
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-04 02:40:47 +0300

    Add order status indicator, for show order status

---
+++ src/app/shared/order-status-indicator/order-status-indicator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-status-indicator',
+  templateUrl: './order-status-indicator.component.html',
+  styleUrls: ['./order-status-indicator.component.scss']
+})
+export class OrderStatusIndicatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
