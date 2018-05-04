6c5d99a3ed6d277feb709f99a2b9bb7112c7563d
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-04 23:41:05 +0300

    Update dashboard of company

--- src/app/views/core/companies/company/dashboard/dashboard.component.ts
+++ src/app/views/core/companies/company/dashboard/dashboard.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  
+  public totalList: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.totalList = [
+      { action: 'blue', value: 455, title: 'Заказов', subTitle: 'В этом месяце', subValue: 34, icon: 'shopping_cart' },
+      { action: 'green', value: 200, title: 'Клиентов', subTitle: 'В этом месяце', subValue: 4, icon: 'shopping_cart' },
+      { action: 'yellow', value: '450000 ₽', title: 'Заработано всего', subTitle: 'В этом месяце', subValue: '3400 ₽', icon: 'shopping_cart' },
+      { action: 'pink', value: '250000 ₽', title: 'Заработано чистыми', subTitle: 'В этом месяце', subValue: '400 ₽', icon: 'shopping_cart' },
+    ];
   }
 
 }
