d0db7226cdabdb6cf6984fd6a4e1a56637c02ce0
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-09 15:09:37 +0300

    add get employee and roleslist

--- src/app/views/core/companies/company/employees/employee-item/employee-profile/employee-profile.component.ts
+++ src/app/views/core/companies/company/employees/employee-item/employee-profile/employee-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../../../../../../services';
+import { IUser } from '../../../../../../../interfaces/user';
 
 @Component({
   selector: 'app-employee-profile',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-profile.component.scss']
 })
 export class EmployeeProfileComponent implements OnInit {
-
-  constructor() { }
+  public isLoaded = false;
+  public profile: IUser;
+  public roleList: any[];
+  
+  constructor(
+    private employeeService: EmployeeService
+  ) { }
+  
+  getEmployee(id: string) {
+    this.employeeService.getEmployee(id)
+      .then( res => {
+        console.log(res);
+        this.profile = res.body;
+        this.isLoaded = true;
+      });
+  }
 
   ngOnInit() {
+    this.getEmployee(this.employeeService.employeeId);
+    this.roleList = [
+      {id: 2, name: 'Ст. менеджер'},
+      {id: 3, name: 'Менеджер'},
+      {id: 4, name: 'Монтажник'},
+    ];
   }
 
 }
