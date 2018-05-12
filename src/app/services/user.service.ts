609d5aefb4a440bcc8c68968824bbb2d01318a65
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-12 21:22:01 +0300

    add get user data

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -13,6 +13,10 @@ export class UserService {
   public manager = 3;
   public worker = 4;
 
+  private _user: any[];
+  get user() {return this._user}
+  set user(user: any) {this._user = user}
+
   constructor(
     private router: Router,
     private restService: RestService,
@@ -26,6 +30,21 @@ export class UserService {
     return new Promise((res, rej) => {
       this.restService.get(Endpoint.USER_ROLES)
         .then( data => {
+          res(data);
+        }, err => {
+          rej(err);
+        });
+    });
+  }
+
+  /**
+   * get user info
+   */
+  getUser(): Promise<any> {
+    return new Promise((res, rej) => {
+      this.restService.get(Endpoint.USER_INFO)
+        .then( data => {
+          this.user = data.body;
           res(data);
         }, err => {
           rej(err);
