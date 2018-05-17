c691ffc9508dfd8ba075a1fa5d4e2bcfc9962444
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-18 01:33:17 +0300

    add init rules

--- src/app/views/core/companies/company/settings/settings.component.ts
+++ src/app/views/core/companies/company/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyService, EventService } from '../../../../../services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  private id: string;
+  public navigation: any;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private companyService: CompanyService,
+    private eventService: EventService
+  ) { }
 
   ngOnInit() {
+    this.eventService.broadcast('app-header-back', {link: 'companies'});
+    this.id = this.companyService.companyId;
+    this.navigation = [
+      {
+          link: `common`,
+          name: 'Информация о компании'
+      },
+    ];
+    this.router.navigate([`companies/${this.id}/settings/common`]);
   }
 
 }
