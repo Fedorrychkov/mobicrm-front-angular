258cb24540fc1445f3158108224bce34e245a62a
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-23 21:47:02 +0300

    update order root page for employee

--- src/app/views/core/orders/orders.component.ts
+++ src/app/views/core/orders/orders.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService, EventService, CompanyService, OrderService } from '../../../services';
 
 @Component({
     templateUrl: 'orders.component.html'
 })
 
 export class OrdersComponent implements OnInit {
-    constructor() { }
+    private companyId: string;
+    public orders: any;
+    public isLoaded = false;
 
-    ngOnInit() { }
+    constructor(
+        private eventService: EventService,
+        private companyService: CompanyService,
+        private orderService: OrderService
+    ) { }
+
+    getOrders() {
+        this.orderService.getOrders(this.companyId).then( data => {
+            this.orders = data;
+            this.isLoaded = true;
+        });
+    }
+    ngOnInit() {
+        this.companyId = this.companyService.companyId;
+        this.getOrders();
+        this.eventService.on('order-list-update').subscribe( () => {
+            this.getOrders();
+        });
+    }
 }
--- src/app/views/core/orders/orders.module.ts
+++ src/app/views/core/orders/orders.module.ts
@@ -3,12 +3,44 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersComponent } from './orders.component';
 import { OrdersRoutingModule } from './orders.routing';
+import { OrderListComponent } from './order-list/order-list.component';
+import { NewOrderComponent } from './new-order/new-order.component';
+import { OrderItemComponent } from './order-item/order-item.component';
 
+import { 
+  MatButtonModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatTableModule,
+  MatIconModule,
+  MatTabsModule,
+  MatInputModule,
+  MatSelectModule,
+  MatFormFieldModule } from '@angular/material';
+
+
+const APP_MATERIAL = [
+  MatButtonModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatTableModule,
+  MatIconModule,
+  MatTabsModule,
+  MatInputModule,
+  MatSelectModule,
+  MatFormFieldModule,
+]
 @NgModule({
   imports: [
     OrdersRoutingModule,
-    CommonModule
+    CommonModule,
+    ...APP_MATERIAL,
   ],
-  declarations: [ OrdersComponent ]
+  declarations: [ 
+    OrdersComponent,
+    OrderListComponent,
+    NewOrderComponent,
+    OrderItemComponent,
+  ]
 })
 export class OrdersModule { }
