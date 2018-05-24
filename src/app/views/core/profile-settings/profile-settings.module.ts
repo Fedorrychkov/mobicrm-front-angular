4471c27b96466d672285af898751a18b0197cecc
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-25 02:46:18 +0300

    add shared module, reactive forms

--- src/app/views/core/profile-settings/profile-settings.module.ts
+++ src/app/views/core/profile-settings/profile-settings.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProfileSettingsComponent } from './profile-settings.component';
 import { ProfileSettingsRoutingModule } from './profile-settings.routing';
 import { ProfileCommonComponent } from './profile-common/profile-common.component';
+import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
   imports: [
     ProfileSettingsRoutingModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    SharedModule,
   ],
-  declarations: [ ProfileSettingsComponent, ProfileCommonComponent ]
+  declarations: [ 
+    ProfileSettingsComponent,
+    ProfileCommonComponent
+  ]
 })
 export class ProfileSettingsModule { }
